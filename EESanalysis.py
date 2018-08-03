6238d839e28433081e52b1aceda8aca7cf67c05e
Daniel Strobl <daniel.strobl@univie.ac.at>
2018-08-03 11:07:02 +0200

    reformatting mostly

--- EESanalysis.py
+++ EESanalysis.py
@@ -4,33 +4,37 @@ from EESdata import EES_data
 import pandas as pd
 from numpy import random
 
-party_data = pd.read_csv('data/party_list.csv')
-sfb_data = pd.read_csv('data/SFB_output_data.csv')
-data = party_data.rename(columns={'country_x': 'country'})
+party_data = pd.read_csv("data/party_list.csv")
+sfb_data = pd.read_csv("data/SFB_output_data.csv")
+data = party_data.rename(columns={"country_x": "country"})
 ees_data = EES_data()
 
 # merge party data with voter placements (ees_data):
 for year, df in ees_data.items():
-    for vars in ['means', 'se']:
+    for vars in ["means", "se"]:
         d = ees_data[year].copy().filter(regex=vars)
-        d['country'] = d.index
-        d['year'] = year
-        d = pd.melt(d.rename(columns=lambda x: x.replace('party', '').replace(vars, '')),
-                    id_vars=['country', 'year'], value_name=vars, var_name='ees_partynr')
-        data = pd.merge(data, d, how='left', on=['year', 'country', 'ees_partynr'])
-        xvar, yvar = vars + '_x', vars + '_y'
+        d["country"] = d.index
+        d["year"] = year
+        d = pd.melt(
+            d.rename(columns=lambda x: x.replace("party", "").replace(vars, "")),
+            id_vars=["country", "year"],
+            value_name=vars,
+            var_name="ees_partynr",
+        )
+        data = pd.merge(data, d, how="left", on=["year", "country", "ees_partynr"])
+        xvar, yvar = vars + "_x", vars + "_y"
         if xvar in data.columns:
             data[vars] = data[xvar].fillna(data[yvar])
             data.drop([xvar, yvar], axis=1, inplace=True)
 
-data
 
+cab_data = pd.merge(party_data, sfb_data, on="cab_id")
 
-cab_data = pd.merge(party_data, sfb_data, on='cab_id')
-
-df = pd.DataFrame(random.poisson(sfb_data['rightMeasures'], size=(10000, len(sfb_data))))
-random.poisson(sfb_data['leftMeasures'], size=(10000, len(sfb_data)))
+df = pd.DataFrame(
+    random.poisson(sfb_data["rightMeasures"], size=(10000, len(sfb_data)))
+)
+random.poisson(sfb_data["leftMeasures"], size=(10000, len(sfb_data)))
 
 df = df.unstack()
-df['cab_id'] = df.index
-pd.merge(sfb_data, df.unstack(), on='cab_id')
+df["cab_id"] = df.index
+pd.merge(sfb_data, df.unstack(), on="cab_id")
--- EESdata.py
+++ EESdata.py
@@ -12,16 +12,14 @@ import pandas as pd
 from scipy import stats
 
 
-def prep_data(year, data_files, pos_vars, country_var,
-              country_dict, replace_dict):
+def prep_data(year, data_files, pos_vars, country_var, country_dict, replace_dict):
     """
     prep_data is the basic function to read in the Stata files and output
     a pandas data frame with the means and std. errors of party positions.
     Unfortunately the data set for 1994 is structured differently, so the
     function only applies to 1989, 1999, and 2004.
     """
-    stata = pd.read_stata('data/' + data_files,
-                          columns=pos_vars + [country_var])
+    stata = pd.read_stata("data/" + data_files, columns=pos_vars + [country_var])
     df = pd.DataFrame(index=country_dict.keys())
     column_rename_dict = {}
 
@@ -29,24 +27,22 @@ def prep_data(year, data_files, pos_vars, country_var,
         stata.loc[:, var] = stata.loc[:, var].replace(replace_dict)
         if year == 2004:  # special recoding for SWE 04
             stata.loc[:, var] = stata.loc[:, var].astype(float)
-<<<<<<< HEAD:EESdata.py
-            stata.loc[stata.country == 'sweden', var] = 1 + (stata[var] * 9) / 10
-        df.loc[:, var + 'means'] = stata.groupby(country_var)[var].mean()
-        df.loc[:, var + 'se'] = stata.groupby(country_var)[var].apply(stats.sem,
-                                                                       nan_policy='omit')
+            stata.loc[stata.country == "sweden", var] = 1 + (stata[var] * 9) / 10
+        df.loc[:, var + "means"] = stata.groupby(country_var)[var].mean()
+        df.loc[:, var + "se"] = stata.groupby(country_var)[var].apply(
+            stats.sem, nan_policy="omit"
+        )
 
-        column_rename_dict[var + 'means'] = 'party' + str(i + 1) + 'means'
-        column_rename_dict[var + 'se'] = 'party' + str(i + 1) + 'se'
-=======
-            stata.loc[stata.country == 'sweden',
-                      var] = 1 + (stata[var] * 9) / 10
-        df.loc[:, var + '_mean'] = stata.groupby(country_var)[var].mean()
-        df.loc[:, var + '_se'] = stata.groupby(
-            country_var)[var].apply(stats.sem, nan_policy='omit')
+        column_rename_dict[var + "means"] = "party" + str(i + 1) + "means"
+        column_rename_dict[var + "se"] = "party" + str(i + 1) + "se"
+        stata.loc[stata.country == "sweden", var] = 1 + (stata[var] * 9) / 10
+        df.loc[:, var + "_mean"] = stata.groupby(country_var)[var].mean()
+        df.loc[:, var + "_se"] = stata.groupby(country_var)[var].apply(
+            stats.sem, nan_policy="omit"
+        )
 
-        column_rename_dict[var + '_mean'] = 'party' + str(i + 1) + '_mean'
-        column_rename_dict[var + '_se'] = 'party' + str(i + 1) + '_se'
->>>>>>> d867f65241f7117b7f9236e16edc41bec146c6dc:EES-data.py
+        column_rename_dict[var + "_mean"] = "party" + str(i + 1) + "_mean"
+        column_rename_dict[var + "_se"] = "party" + str(i + 1) + "_se"
 
     # finally, set unified index and column names
     df = df.rename(country_dict, columns=column_rename_dict)
@@ -56,44 +52,83 @@ def prep_data(year, data_files, pos_vars, country_var,
 
 def EES_data():
     # define the relevant files, variables and how they should be recoded
-    data_files = {1989: 'ZA2320.dta',
-                  1994: 'ZA2865.dta',
-                  1999: 'P1560a.dta',
-                  2004: 'ZA4566.dta'}
-    pos_vars = {1989: ['var' + str(x) for x in range(203, 213)],
-                1994: ['v' + str(x) for x in range(118, 127)],
-                1999: ['var' + str(x) for x in range(118, 130)],
-                2004: ['v' + str(x) for x in range(135, 143)]}
-    country_vars = {1989: 'var003',
-                    1999: 'var002',
-                    2004: 'country'}
-    country_dict = {1989: {'DK': 'DK', 'FR': 'FR', 'GER': 'GER',
-                           'IRE': 'IRE', 'NL': 'NL', 'GB': 'GB'},
-                    1994: {'den': 'DK', 'fra': 'FR', 'wge': 'GER', 'gb': 'GB',
-                           'irl': 'IRE', 'net': 'NL'},
-                    1999: {'Denmark': 'DK', 'France': 'FR', 'Germany': 'GER',
-                           'UK': 'GB', 'Ireland': 'IRE', 'Netherlands': 'NL',
-                           'Austria': 'AT', 'Sweden': 'SWE'},
-                    2004: {'denmark': 'DK', 'france': 'FR', 'germany': 'GER',
-                           'britain': 'GB', 'ireland': 'IRE', 'netherlands': 'NL',
-                           'austria': 'AT', 'sweden': 'SWE'}}
-    replace_dict = {1989: {'LEFT': 1.0, 'RIGHT': 10.0},
-                    1994: {'LEFT': 1.0, 'RIGHT': 10.0, 'DK': None,
-                           'NA': None, 99.0: None},
-                    1999: {'left': 1.0, 'right': 10.0, 'dk': None,
-                           -1.0: None, 'na': None},
-                    2004: {'an 11-point scale (0-10) was used in SE': 0.0,
-                           'left': 1.0, 'right': 10.0, 'd/k, n/a': None}}
+    data_files = {
+        1989: "ZA2320.dta",
+        1994: "ZA2865.dta",
+        1999: "P1560a.dta",
+        2004: "ZA4566.dta",
+    }
+    pos_vars = {
+        1989: ["var" + str(x) for x in range(203, 213)],
+        1994: ["v" + str(x) for x in range(118, 127)],
+        1999: ["var" + str(x) for x in range(118, 130)],
+        2004: ["v" + str(x) for x in range(135, 143)],
+    }
+    country_vars = {1989: "var003", 1999: "var002", 2004: "country"}
+    country_dict = {
+        1989: {
+            "DK": "DK",
+            "FR": "FR",
+            "GER": "GER",
+            "IRE": "IRE",
+            "NL": "NL",
+            "GB": "GB",
+        },
+        1994: {
+            "den": "DK",
+            "fra": "FR",
+            "wge": "GER",
+            "gb": "GB",
+            "irl": "IRE",
+            "net": "NL",
+        },
+        1999: {
+            "Denmark": "DK",
+            "France": "FR",
+            "Germany": "GER",
+            "UK": "GB",
+            "Ireland": "IRE",
+            "Netherlands": "NL",
+            "Austria": "AT",
+            "Sweden": "SWE",
+        },
+        2004: {
+            "denmark": "DK",
+            "france": "FR",
+            "germany": "GER",
+            "britain": "GB",
+            "ireland": "IRE",
+            "netherlands": "NL",
+            "austria": "AT",
+            "sweden": "SWE",
+        },
+    }
+    replace_dict = {
+        1989: {"LEFT": 1.0, "RIGHT": 10.0},
+        1994: {"LEFT": 1.0, "RIGHT": 10.0, "DK": None, "NA": None, 99.0: None},
+        1999: {"left": 1.0, "right": 10.0, "dk": None, -1.0: None, "na": None},
+        2004: {
+            "an 11-point scale (0-10) was used in SE": 0.0,
+            "left": 1.0,
+            "right": 10.0,
+            "d/k, n/a": None,
+        },
+    }
 
     # run pred_data function for 1989, 1999, and 2004
     pos_data = {}
     for yr in country_vars.keys():
-        pos_data[yr] = prep_data(yr, data_files[yr], pos_vars[yr],
-                                 country_vars[yr], country_dict[yr],
-                                 replace_dict[yr])
+        pos_data[yr] = prep_data(
+            yr,
+            data_files[yr],
+            pos_vars[yr],
+            country_vars[yr],
+            country_dict[yr],
+            replace_dict[yr],
+        )
 
     # EES dataset 1994
-    ees94 = pd.read_stata('data/' + data_files[1994])
+    ees94 = pd.read_stata("data/" + data_files[1994])
 
     # the basic logic of this loop is to collect all means and std.errors
     # for one country in the 'pc' pd.Series before appending it to the
@@ -103,24 +138,22 @@ def EES_data():
         pc = pd.Series(name=cval)
         column_rename_dict = {}
         for i, var in enumerate(pos_vars[1994]):
-            varname = var + '_' + ckey
+            varname = var + "_" + ckey
             if varname in ees94.keys():
                 ees94.loc[:, varname] = ees94[varname].replace(replace_dict[1994])
-                m = pd.Series(ees94[varname].mean(), index=[var + 'means'], name=cval)
-                se = pd.Series(stats.sem(ees94[varname], nan_policy='omit'),
-                               index=[var + 'se'], name=cval)
+                m = pd.Series(ees94[varname].mean(), index=[var + "means"], name=cval)
+                se = pd.Series(
+                    stats.sem(ees94[varname], nan_policy="omit"),
+                    index=[var + "se"],
+                    name=cval,
+                )
                 pc = pd.concat([pc, m, se])
-<<<<<<< HEAD:EESdata.py
-            column_rename_dict[var + 'means'] = 'party' + str(i + 1) + 'means'
-            column_rename_dict[var + 'se'] = 'party' + str(i + 1) + 'se'
-=======
-            column_rename_dict[var + '_mean'] = 'party' + str(i + 1) + '_mean'
-            column_rename_dict[var + '_se'] = 'party' + str(i + 1) + '_se'
-
->>>>>>> d867f65241f7117b7f9236e16edc41bec146c6dc:EES-data.py
+            column_rename_dict[var + "means"] = "party" + str(i + 1) + "means"
+            column_rename_dict[var + "se"] = "party" + str(i + 1) + "se"
+            column_rename_dict[var + "_mean"] = "party" + str(i + 1) + "_mean"
+            column_rename_dict[var + "_se"] = "party" + str(i + 1) + "_se"
         pos_data[1994] = pos_data[1994].append(pc)
 
-    pos_data[1994] = pos_data[1994].rename(
-        country_dict, columns=column_rename_dict)
+    pos_data[1994] = pos_data[1994].rename(country_dict, columns=column_rename_dict)
 
     return pos_data
